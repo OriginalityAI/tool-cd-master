e963471a6c23a44718252928fb2f580c6d00fcb8
James Ball <jamie.j.ball@gmail.com>
2024-07-26 10:26:57 -0400

    Add new bots to migration

--- backend/db/migrations/20231012203215_add_bots.php
+++ backend/db/migrations/20231012203215_add_bots.php
@@ -6,19 +6,29 @@ use Phinx\Migration\AbstractMigration;
 
 final class AddBots extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change(): void
-    {
-        $this->execute('INSERT INTO Bots (name) VALUES ("GPTBot"), ("CCBot"), ("Google-Extended"), ("anthropic-ai")');
-    }
+  /**
+   * Change Method.
+   *
+   * Write your reversible migrations using this method.
+   *
+   * More information on writing migrations is available here:
+   * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+   *
+   * Remember to call "create()" or "update()" and NOT "save()" when working
+   * with the Table class.
+   */
+  public function change(): void
+  {
+    $this->execute('INSERT INTO Bots (name) VALUES
+        ("GPTBot"),
+        ("CCBot"),
+        ("Google-Extended"),
+        ("anthropic-ai"),
+        ("ChatGPT-User"),
+        ("Googlebot"),
+        ("ClaudeBot"),
+        ("FacebookBot"),
+        ("cohere-ai")
+    ');
+  }
 }
